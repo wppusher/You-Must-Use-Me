2b9500b52e760ba8dcb9fbb66185900aef2fc317
Peter Suhm <peter@suhm.dk>
2017-03-31 10:34:44 +0200

    initial commit

---
+++ must-use.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Plugin Name: You Must Use Me
+ * Plugin URI: https://github.com/wppusher/You-Must-Use-me
+ * Description: This plugin MUST be used.
+ * Version: 1.0.0
+ * Author: WP Pusher
+ * Author URI: http://wppusher.com
+ * License: GNU GENERAL PUBLIC LICENSE
+ */
